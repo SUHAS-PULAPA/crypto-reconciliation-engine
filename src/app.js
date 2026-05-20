51a1853348e3a2af358e4b87cd06db56ce889b1d
suhas <suhas6998@gmail.com>
2026-05-20 19:24:25 +0530

    Modified app.js

--- src/app.js
+++ src/app.js
@@ -12,6 +12,8 @@ mongoose.connect(process.env.MONGO_URI);
 app.use('/', routes);
 app.get('/', (req, res) => {
   res.send('🚀 Crypto Reconciliation Engine is running!');
+  res.send('Check the Readme for API documentation: https://github.com/SUHAS-PULAPA/crypto-reconciliation-engine');
+  res.send('For API Testing use Postman collection: https://www.postman.com/');
 });
 
 module.exports = app;
